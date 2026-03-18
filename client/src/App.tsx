65d44c5f827a6e6bca89e8b6d9dc222c2080c637
chocobanjiumi <chocoban.jiumi@gmail.com>
2026-03-18 14:48:07 +0800

    fix: use /callback.html for popup redirectUri (static page won't clear URL params)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,7 +12,7 @@ export const arinova = new Arinova({
   appId: ARINOVA_APP_ID,
   endpoint: ARINOVA_ENDPOINT,
   scope: 'profile agents economy',
-  redirectUri: window.location.origin + '/mud/',
+  redirectUri: window.location.origin + '/callback.html',
 });
 
 export default function App() {
